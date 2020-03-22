783e7f603840964ae1be8e8af59d31308c1b8584
Bill Hass <billhass@umich.edu>
2020-03-22 15:37:30 -0400

    Improved install script & added more Added a 7zopen and 7zclose helper script with associated bash aliases in .bashrc

--- append_bashrc.sh
+++ append_bashrc.sh
@@ -8,3 +8,13 @@ if ! grep "alias open" ~/.bashrc; then
   echo "alias open=xdg-open"
   echo "alias open=xdg-open" >> ~/.bashrc
 fi
+
+if ! grep "alias 7zopen" ~/.bashrc; then
+  echo "alias 7zopen=~/bin/7zopen.sh"
+  echo "alias 7zopen=~/bin/7zopen.sh" >> ~/.bashrc
+fi
+
+if ! grep "alias 7zclose" ~/.bashrc; then
+  echo "alias 7zclose=~/bin/7zclose.sh"
+  echo "alias 7zclose=~/bin/7zclose.sh" >> ~/.bashrc
+fi
